00e07c1d5ebae07646bf5bbf9b60a5d2a6edfcb8
Keith Radford <khr8771@gmail.com>
2023-02-18 13:13:39 -0700

    Initialize GraphQL backend

---
+++ backend/api/graphql/Post.ts
@@ -0,0 +1,12 @@
+import { objectType } from "nexus";
+
+export const Post = objectType({
+  name: "Post", // <- Name of your type
+
+  definition(t) {
+    t.int("id"); // <- Field named `id` of type `Int`
+    t.string("title"); // <- Field named `title` of type `String`
+    t.string("body"); // <- Field named `body` of type `String`
+    t.boolean("published"); // <- Field named `published` of type `Boolean`
+  },
+});

5385bc43b99733e920af7f40c0b393e253a81d60
Keith Radford <khr8771@gmail.com>
2023-02-18 19:58:59 -0700

    UI Changes

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -23,13 +23,8 @@ export default function Graph() {
   return (
     <Center my="5" w="100%" h="500px">
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={600}
-          height={300}
-          data={data}
-          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-        >
-          <Line type="monotone" dataKey="uv" stroke="#572c56" />
+        <LineChart width={600} height={300} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#cb92bf" strokeWidth={2} />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="name" />
           <YAxis />
--- frontend/src/components/Selects.tsx
+++ frontend/src/components/Selects.tsx
@@ -1,59 +1,18 @@
-import { Box, HStack, useRadio, useRadioGroup } from "@chakra-ui/react";
-
-function RadioCard(props: any) {
-  const { getInputProps, getCheckboxProps } = useRadio(props);
-
-  const input = getInputProps();
-
-  const checkbox = getCheckboxProps();
-
-  return (
-    <Box as="label">
-      <input {...input} />
-      <Box
-        {...checkbox}
-        cursor="pointer"
-        color="white"
-        bg="black"
-        backgroundSize="200% auto"
-        borderRadius="lg"
-        boxShadow="md"
-        _checked={{
-          bg: "linear-gradient(107deg, #9733EE 0%, #DA22FF  51%)",
-          color: "white",
-          boxShadow: "none",
-        }}
-        px="10px"
-        py="5px"
-      >
-        {props.children}
-      </Box>
-    </Box>
-  );
-}
+import { Box, Select } from "@chakra-ui/react";
 
 export default function Selects() {
-  const options = ["Educated Men", "Educated Women", "Gog"];
-
-  const { getRootProps, getRadioProps } = useRadioGroup({
-    name: "selections",
-    defaultValue: options[0],
-    onChange: console.log,
-  });
-
-  const group = getRootProps();
-
   return (
-    <HStack {...group} p="10px">
-      {options.map((value) => {
-        const radio = getRadioProps({ value });
-
-        return (
-          <RadioCard key={value} {...radio}>
-            {value}
-          </RadioCard>
-        );
-      })}
-    </HStack>
+    <Box
+      mt="5"
+      bgGradient="linear-gradient(to right, #8377D1 10%, #cb92bf )"
+      borderRadius="lg"
+      color="white"
+    >
+      <Select placeholder="Select option" border="none">
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </Select>
+    </Box>
   );
 }

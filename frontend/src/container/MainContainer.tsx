5376ee8c6c9944e26ae56dd735aedca9911ef71b
Kaleb Chisholm <46547298+Kaleb-Chisholm@users.noreply.github.com>
2023-02-19 03:16:24 -0700

    sliders track values

--- frontend/src/container/MainContainer.tsx
+++ frontend/src/container/MainContainer.tsx
@@ -4,8 +4,16 @@ import PercentWheel from "../components/PercentWheel";
 import Selects from "../components/Selects";
 import Sliders from "../components/Sliders";
 import InfoButton from "../components/InfoButton";
+import { useState } from "react";
 
 export default function MainContainer() {
+  const [sliderValue1, setSliderValue1] = useState(1);
+  const [sliderValue2, setSliderValue2] = useState(1);
+  const [sliderValue3, setSliderValue3] = useState(1);
+  const handleSlider1 = (val:any) => {setSliderValue1(val)}
+  const handleSlider2 = (val:any) => {setSliderValue2(val)}
+  const handleSlider3 = (val:any) => {setSliderValue3(val)}
+
   return (
     <Box borderRadius="lg">
       <Grid
@@ -22,7 +30,14 @@ export default function MainContainer() {
         </GridItem>
         <GridItem pl="2" area={"select"}>
           <Selects />
-          <Sliders />
+          <Sliders
+            handleSlider1={handleSlider1}
+            handleSlider2={handleSlider2}
+            handleSlider3={handleSlider3}
+            sliderValue1={sliderValue1}
+            sliderValue2={sliderValue2}
+            sliderValue3={sliderValue3}
+          />
         </GridItem>
         <GridItem pl="2" area={"footer"}>
           <PercentWheel />

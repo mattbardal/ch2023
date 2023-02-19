f4dcaae0313736806ac19dda5e84949a9c239ef5
Keith Radford <khr8771@gmail.com>
2023-02-18 17:40:55 -0700

    Gradient swag

--- frontend/src/components/Selects.tsx
+++ frontend/src/components/Selects.tsx
@@ -1,18 +1,13 @@
 export default function Selects() {
-
-  const tempData = [
-    'select1',
-    'select2',
-    'select3'
-  ]
+  const tempData = ["select1", "select2", "select3"];
 
   return (
     <div>
-      {
+      {/* {
         tempData.map((d, index) => (
           <p key={index}>{d}</p>
         ))
-      }
+      } */}
     </div>
-  )
+  );
 }
--- frontend/src/components/Sliders.tsx
+++ frontend/src/components/Sliders.tsx
@@ -1,18 +1,36 @@
-export default function Sliders() {
+import {
+  Box,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
-  const tempData = [
-    'slider1',
-    'slider2',
-    'slider3'
-  ]
-
+const CustomSlider = () => {
   return (
-    <div>
-      {
-        tempData.map((d, index) => (
-          <p key={index}>{d}</p>
-        ))
-      }
-    </div>
-  )
+    <>
+      <Text fontWeight="bold" fontSize="xl">
+        cool 1
+      </Text>
+      <Box p="3" bgColor="grey.50" borderRadius="full" shadow="lg">
+        <Slider aria-label="slider-ex-2" defaultValue={30}>
+          <SliderTrack>
+            <SliderFilledTrack bg="linear-gradient(to right, #cb63cb 55%,#292541)" />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+      </Box>
+    </>
+  );
+};
+
+export default function Sliders() {
+  return (
+    <VStack w="100%" h="125px" mt="5" alignItems="left" spacing="10px">
+      <CustomSlider />
+      <CustomSlider />
+    </VStack>
+  );
 }

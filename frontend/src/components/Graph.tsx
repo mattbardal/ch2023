067b1f66b044d9746230fab6b6e34ba564ad6e8f
Keith Radford <khr8771@gmail.com>
2023-02-18 20:51:42 -0700

    Error on graph

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -7,16 +7,17 @@ import {
   YAxis,
   Tooltip,
   ResponsiveContainer,
+  ErrorBar,
 } from "recharts";
 
 // generate some random data
 const data = [
-  { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 300, pv: 4567, amt: 2400 },
-  { name: "Page C", uv: 300, pv: 1398, amt: 2400 },
-  { name: "Page D", uv: 200, pv: 9800, amt: 2400 },
-  { name: "Page E", uv: 278, pv: 3908, amt: 2400 },
-  { name: "Page F", uv: 189, pv: 4800, amt: 2400 },
+  { name: "Page A", uv: 400, pv: 2400 / 10, amt: 240, errorY: 10, errorX: 10 },
+  { name: "Page B", uv: 300, pv: 4567 / 10, amt: 2400, errorY: 10, errorX: 10 },
+  { name: "Page C", uv: 300, pv: 1398 / 10, amt: 2400, errorY: 10, errorX: 10 },
+  { name: "Page D", uv: 200, pv: 9800 / 10, amt: 2400, errorY: 10, errorX: 10 },
+  { name: "Page E", uv: 278, pv: 3908 / 10, amt: 2400, errorY: 10, errorX: 10 },
+  { name: "Page F", uv: 189, pv: 4800 / 10, amt: 2400, errorY: 10, errorX: 10 },
 ];
 
 export default function Graph() {
@@ -24,7 +25,16 @@ export default function Graph() {
     <Center my="5" w="100%" h="500px">
       <ResponsiveContainer width="100%" height="100%">
         <LineChart width={600} height={300} data={data}>
-          <Line type="monotone" dataKey="uv" stroke="#cb92bf" strokeWidth={2} />
+          <Line type="monotone" dataKey="uv" stroke="#5a224e" strokeWidth={2}>
+            <ErrorBar
+              dataKey="errorY"
+              width={4}
+              strokeWidth={2}
+              stroke="green"
+              direction="y"
+            />
+          </Line>
+
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="name" />
           <YAxis />

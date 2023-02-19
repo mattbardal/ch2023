6297ba1b313c8841421945da476acaa9fb8ca1b0
Keith Radford <khr8771@gmail.com>
2023-02-18 19:34:26 -0700

    Add graph

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -1,9 +1,41 @@
-import { Box } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+// generate some random data
+const data = [
+  { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+  { name: "Page B", uv: 300, pv: 4567, amt: 2400 },
+  { name: "Page C", uv: 300, pv: 1398, amt: 2400 },
+  { name: "Page D", uv: 200, pv: 9800, amt: 2400 },
+  { name: "Page E", uv: 278, pv: 3908, amt: 2400 },
+  { name: "Page F", uv: 189, pv: 4800, amt: 2400 },
+];
 
 export default function Graph() {
   return (
-    <Box bg="linear-gradient(to right, #cb63cb 55%,#292541)" h="350px">
-      Graph
-    </Box>
+    <Center my="5" w="100%" h="500px">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={600}
+          height={300}
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="uv" stroke="#572c56" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </Center>
   );
 }

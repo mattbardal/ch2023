ba7b773d07733fdacbb99984a8dae70d4be8a1d4
Kaleb Chisholm <46547298+Kaleb-Chisholm@users.noreply.github.com>
2023-02-18 19:23:18 -0700

    small css changes

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -1,9 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 export default function Graph() {
   return (
     <Box bg="linear-gradient(to right, #cb63cb 55%,#292541)" h="350px">
-      Graph
+      <Image src="./images/Cratfield_population_line_graph.jpg" alt="" h='350px' w='100pc'/>
     </Box>
   );
 }
